b275d137b25c109eb64ee9ad96718ded698843ea
Grace <gcw.chung@gmail.com>
2021-05-04 16:37:26 -0400

    Implemented landing page main text

---
+++ client/src/components/page/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Landing.css'
+
+
+export default function Landing() {
+    return (
+        <div className="landing-container">
+            <div className='landing-wrapper'>
+                <div className="landing-year">2021</div> 
+                <div className="landing-main">The Shoppies 
+                    <div className="sub">
+                        <span>Mov</span>i<span>e</span> Awar<span>d</span>s
+                    </div>
+                </div>
+            </div>
+             <div className="landing-sub"> Nominate your top 5 movies </div>
+        </div>
+
+
+
+    )
+}
