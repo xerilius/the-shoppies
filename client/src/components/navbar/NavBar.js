456bc6c70aa420fe5e69d14a6404fe9d13e453e5
Grace <gcw.chung@gmail.com>
2021-05-04 16:39:02 -0400

    Implemented NavBar

---
+++ client/src/components/navbar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NavBar.css'
+
+
+export default function NavBar() {
+    return (
+        <>
+        <h2 className="nav">
+            <a href="/">The Shoppies</a>
+        </h2>
+        </>
+    )
+}
