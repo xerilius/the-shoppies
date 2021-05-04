7f146495674ee78c503ed82e447793c929ba5932
Grace <gcw.chung@gmail.com>
2021-05-04 16:40:27 -0400

    Added SearchBar, NavBar and Landing page text

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import NavBar from './components/navbar/NavBar'
+import Landing from './components/page/Landing'
+import SearchBar from './components/searchbar/SearchBar'
+import './App.css'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route path="/">
+        <Switch>
+          <div className="page-container">
+            <NavBar />
+            <Landing />
+            <SearchBar />
+          </div>
+        </Switch>
+     </Route>
+    </Router>
+  )
 }
 
-export default App;
+export default App
